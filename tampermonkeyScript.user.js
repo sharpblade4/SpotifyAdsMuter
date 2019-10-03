1387d99c75f3a822893f52446de7e53049186a97
Ron U <sharpblade4@gmail.com>
2019-10-03 14:29:32 +0300

    stabilizing unmute

--- tampermonkeyScript.user.js
+++ tampermonkeyScript.user.js
@@ -17,7 +17,7 @@ const UNMUTE_ARIA_LABEL = "Unmute";
 const ENABLED_VOL_BAR_CLASS = "volume-bar";
 
 var g_artificialMute = false;
-
+var g_muteLastClassName = "";
 
 function isAdBeingPlayed() {
     const nextButtons = jQuery('[title='+NEXT_TITLE+']');
@@ -30,27 +30,39 @@ function isAdBeingPlayed() {
 }
 
 
+function isMuteOn() {
+    const unmuteButtons = jQuery('[aria-label='+UNMUTE_ARIA_LABEL+']');
+    return (unmuteButtons.length == 0); // if unmute buttons are shown then mute is on
+}
+
+
 function applyArtificialMute() {
     const muteButtons = jQuery('[aria-label='+MUTE_ARIA_LABEL+']');
     for (var j = 0; j < muteButtons.length; j++) {
         muteButtons[j].click();
         g_artificialMute = true;
+        g_muteLastClassName = muteButtons[j].className;
         return;
     }
 }
 
 function applyArtificialUnmute() {
-    const unmuteButtons = jQuery('[aria-label='+UNMUTE_ARIA_LABEL+']');
-    for (var i = 0; i < unmuteButtons.length; i++) {
-        unmuteButtons[i].parentElement.className = ENABLED_VOL_BAR_CLASS;
-        unmuteButtons[i].click();
+    if (!isMuteOn()) { // already unmuted
         g_artificialMute = false;
-        return;
+    } else {
+        const unmuteButtons = jQuery('[aria-label='+UNMUTE_ARIA_LABEL+']');
+        for (var i = 0; i < unmuteButtons.length; i++) {
+            unmuteButtons[i].parentElement.className = ENABLED_VOL_BAR_CLASS;
+            unmuteButtons[i].click();
+            unmuteButtons[i].setAttribute('aria-label', MUTE_ARIA_LABEL);
+            unmuteButtons[i].className = g_muteLastClassName;
+            return;
+        }
     }
 }
 
 function executeAdsTestAndVolControl() {
-    if (isAdBeingPlayed()) {
+    if (isAdBeingPlayed() && !isMuteOn()) {
         applyArtificialMute();
     } else if (g_artificialMute) { // no ad but artificially muted
         applyArtificialUnmute();
